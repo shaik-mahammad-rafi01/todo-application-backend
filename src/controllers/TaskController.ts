b377bb02aa6b32495fddf12d60bde62bd037296f
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-11-18 23:16:51 +0530

    Add controller for create a new task

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -1,6 +1,6 @@
 import type { Request, Response } from "express";
-import { getAllTasks } from "../services/TaskServices.js";
-
+import { addTask, getAllTasks } from "../services/TaskServices.js";
+import type { Task } from "../types/Task.js";
 
 export const getTask = async (req: Request, res: Response) => {
   try {
@@ -10,3 +10,9 @@ export const getTask = async (req: Request, res: Response) => {
     res.status(500).json({error : "Failed to fetch tasks" });
   }
 };
+
+export const createTask = async (req: Request, res: Response) => {
+    const task:Task = req.body;
+    await addTask(task);
+    res.send("Task added successfully");
+}
