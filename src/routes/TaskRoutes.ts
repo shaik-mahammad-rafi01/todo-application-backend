15e59522bfcdad2b28826417d0295cdce07d6d27
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-11-18 23:40:46 +0530

    Add createtask route to post new task

--- src/routes/TaskRoutes.ts
+++ src/routes/TaskRoutes.ts
@@ -1,7 +1,8 @@
 import express, { Router } from "express";
-import {  getTask } from "../controllers/TaskController.js";
+import { createTask, getTask } from "../controllers/TaskController.js";
 const taskRouter = Router();
 
 taskRouter.get("/task" , getTask)
+taskRouter.post("/task" , createTask)
 
 export default taskRouter;
