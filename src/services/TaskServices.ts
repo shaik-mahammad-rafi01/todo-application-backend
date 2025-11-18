10e53130f4b8354aacabcbec1f9002cab832667b
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-11-18 23:04:19 +0530

    Implement addTask service to add new task to db

--- src/services/TaskServices.ts
+++ src/services/TaskServices.ts
@@ -3,6 +3,10 @@ import type { Task } from "../types/Task.js";
 
 const taskCollection = db.collection("Tasks");
 
+export const addTask = async(task:Task)=>{
+    await taskCollection.add(task)
+}
+
 export const getAllTasks = async () => {
     const items = await taskCollection.get();
     return items.docs.map(doc => ({ id: doc.id, ...doc.data() }));
