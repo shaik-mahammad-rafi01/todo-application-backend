97b97bf3c96d8fb30053bf5df703aafef7d4d0f8
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-11-20 00:06:29 +0530

    Install required depencey for frontend integration

--- app.ts
+++ app.ts
@@ -1,9 +1,11 @@
 import express  from 'express';
 import taskRouter from './src/routes/TaskRoutes.js';
+import cors from "cors";
 
 const app = express()
 app.use(express.json())
 const port = 3005
+app.use(cors());
 app.use("/" , taskRouter)
 
 app.listen(port, () => {
