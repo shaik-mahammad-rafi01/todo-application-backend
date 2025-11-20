5b74713110e83329d1cf1b0502f4d918386f9dcb
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-11-20 22:41:55 +0530

    Add put route for updating a task

--- src/routes/TaskRoutes.ts
+++ src/routes/TaskRoutes.ts
@@ -1,9 +1,10 @@
 import express, { Router } from "express";
-import { createTask, deleteTask, getTask } from "../controllers/TaskController.js";
+import { createTask, deleteTask, getTask, putExpense } from "../controllers/TaskController.js";
 const taskRouter = Router();
 
 taskRouter.get("/task" , getTask)
 taskRouter.post("/task" , createTask)
 taskRouter.delete("/task/:id" , deleteTask)
+taskRouter.put("/task/:id", putExpense)
 
 export default taskRouter;
