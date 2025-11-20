86471a1f7da5346a93689f7c7fff5c59933472f5
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-11-20 16:37:47 +0530

    Add deleteTaskInDb function to remove task by id from db

--- src/services/TaskServices.ts
+++ src/services/TaskServices.ts
@@ -3,11 +3,18 @@ import type { Task } from "../types/Task.js";
 
 const taskCollection = db.collection("Tasks");
 
-export const addTask = async(task:Task)=>{
-    await taskCollection.add(task)
-}
+export const addTask = async (task:Task) => {
+    const newTask = await taskCollection.add(task);
+    return { id: newTask.id, ...task };
+};
 
 export const getAllTasks = async () => {
     const items = await taskCollection.get();
     return items.docs.map(doc => ({ id: doc.id, ...doc.data() }));
 };
+
+export const deleteTaskInDb = async(taskId : string)=>{
+    const id = taskId;
+    console.log(id)
+    return await taskCollection.doc(id).delete()
+}
