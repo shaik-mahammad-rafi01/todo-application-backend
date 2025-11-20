8e9c139664eff173169f28d8c7922d77d866ee4a
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-11-20 16:39:47 +0530

    Add delete route in TaskRoutes

--- src/routes/TaskRoutes.ts
+++ src/routes/TaskRoutes.ts
@@ -1,8 +1,9 @@
 import express, { Router } from "express";
-import { createTask, getTask } from "../controllers/TaskController.js";
+import { createTask, deleteTask, getTask } from "../controllers/TaskController.js";
 const taskRouter = Router();
 
 taskRouter.get("/task" , getTask)
 taskRouter.post("/task" , createTask)
+taskRouter.delete("/task/:id" , deleteTask)
 
 export default taskRouter;
